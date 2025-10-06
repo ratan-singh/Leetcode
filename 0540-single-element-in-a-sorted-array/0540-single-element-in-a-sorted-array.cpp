2b1dfd2cda9b4ebb5a1b4ca7cb4eaf3104d21801
Ratan Singh <125920141+ratan-singh@users.noreply.github.com>
2025-10-06 13:40:24 +0530

    Time: 0 ms (100%), Space: 26.3 MB (34.01%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int l=0, h=n-1;
+
+        while(l<h){
+
+            int mid = l+(h-l)/2;
+
+            //right side me even hai yaa odd hai
+            bool isEven;
+
+            if((h-mid)%2 == 0){
+                isEven = true;
+            }else{
+                isEven = false;
+            }
+
+            //check your bagal wla element
+            if(nums[mid] == nums[mid+1]){
+                if(isEven){
+                    l = mid+2;
+                }else{
+                    h = mid-1;
+                }
+            }else{//nums[mid] != nums[mid+1] {., ., ., a, a, b, b, c}
+                if(isEven){
+                    h = mid;
+                }else{
+                    l = mid+1;
+                }
+            }
+        }
+        return nums[h];
+    }
+};
